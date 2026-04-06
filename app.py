388bfa57fb1ae966c03f38024662a1a126ed756f
Anu A <167222325+anu-2024@users.noreply.github.com>
2026-04-06 23:08:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,14 @@ from env import WasteEnv, Action
 app = FastAPI()
 env = WasteEnv()
 
-@app.get("/")
-def root():
-    return {"message": "Smart Waste OpenEnv API running"}
-
 @app.get("/reset")
+@app.post("/reset")
 def reset():
     obs = env.reset()
     return obs.dict()
 
 @app.get("/state")
+@app.post("/state")
 def state():
     return env.state()
 
